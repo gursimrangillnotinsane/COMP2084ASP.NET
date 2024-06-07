21b5b57fe51c7bd7524e664a7b60f86b92b08ae3
gursimrangillnotinsane <132660017+gursimrangillnotinsane@users.noreply.github.com>
2024-06-07 18:51:54 -0400

    Initial commit for lab02

---
+++ LabWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using LabWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabWebApp.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Product> Product {  get; set; }
+    }
+}
